85be0339530b25a98452497ec181f8c0bb628053
ayoub khatouri <ayoub.khatouri@usmba.ac.ma>
2023-12-01 23:13:24 +0100

    build image for config-service

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class GatewayServiceApplication {
+public class
+GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
